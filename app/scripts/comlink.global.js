6136e992425d0dc817b46623ea1b221dd6309b4e
Saheed Oladosu <38882296+kodeGenius@users.noreply.github.com>
2020-07-18 23:07:24 +0100

    Update comlink.global.js

--- app/scripts/comlink.global.js
+++ app/scripts/comlink.global.js
@@ -11,6 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// callback function
 
 self.Comlink = (function () {
     const uid = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
